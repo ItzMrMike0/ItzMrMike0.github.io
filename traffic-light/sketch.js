24dd6cd82160295872631c241f358c1e3f5eb75b
Michael Yang <michaelisanime@gmail.com>
2024-09-24 14:14:44 -0600

    Traffic Light Started

---
+++ traffic-light/sketch.js
@@ -0,0 +1,59 @@
+// Traffic Light Starter Code
+// Michael Yang
+// 9/24/2024
+
+// GOAL: make a 'traffic light' simulator. For now, just have the light
+// changing according to time. You may want to investigate the millis()
+// function at https://p5js.org/reference/#/p5/millis
+
+let state = "red"
+let waitTime = 4000;
+let lastSwitchedTime = 0;
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(255);
+  drawOutlineOfLights();
+}
+
+function drawOutlineOfLights() {
+  //box
+  rectMode(CENTER);
+  fill(0);
+  rect(width/2, height/2, 75, 200, 10);
+
+  //lights
+  fill(255);
+  
+  changeLight();
+  ellipse(width/2, height/2 - 65, 50, 50); //top
+  ellipse(width/2, height/2, 50, 50); //middle
+  ellipse(width/2, height/2 + 65, 50, 50); //bottom
+}
+
+function changeLight() {
+  if (state === "red") {
+    fill("red");
+    if (millis() >= lastSwitchedTime + waitTime) {
+      lastSwitchedTime = millis();
+      state = "yellow"
+    }
+  }
+  else if (state === "yellow"){
+    fill("yellow");
+    if (millis() >= lastSwitchedTime + waitTime/2) {
+      lastSwitchedTime = millis();
+      state = "green"
+  }
+}
+  else if (state === "green"){
+    fill("green");
+    if (millis() >= lastSwitchedTime + waitTime) {
+      lastSwitchedTime = millis()
+      state = "red"
+  }
+}
+}
