ec258b63fe7a2212fe9a88ac355b8ad4b3a14e63
Michael Yang <michaelisanime@gmail.com>
2024-09-23 19:55:46 -0600

    Completely reworked mouseClicked and state variables, alongside this I finally got scaling to work.

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -5,49 +5,44 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
-// 0 = Title Screen, 1 = Select Screen, 2 = Timing Game Instructions, 3 = Number Game Instructions
-let gameMode = 0;
+// Variables
+let gameState = "title";; // Possible states: "title", "select", "timerIns", "timer", "number"
 let thunderImg;
 
+//Canvas Setup
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+// Images
 function preload() {
-  thunderImg = loadImage('thunderbolt.png');
+  thunderImg = loadImage("thunderbolt.png");
 }
 
-// Changing gameMode
+// Handle Window Resizing
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight); 
+}
+
+// Changing Screen/State
 function mouseClicked() {
   // Title Screen to Game Mode Selection
-  if (gameMode === 0) {
-    gameMode += 1;
+  if (gameState === "title") {
+    gameState = "select";
   }
   // Timing Game
-  else if (gameMode === 1) {
-    if (
-      mouseX > 0 &&
-      mouseX < width / 2 &&
-      mouseY > height / 3.5 &&
-      mouseY < 2 * height
-    ) {
-      gameMode += 1;
+  else if (gameState === "select") {
+    if (mouseX > 0 && mouseX < width / 2 && mouseY > height / 3.5 && mouseY < 2 * height) {
+      gameState = "timerIns"
       // Number Game
-    } else {
-      if (
-        mouseX > width / 2 &&
-        mouseX < width + width / 2 &&
-        mouseY > height / 3.5 &&
-        mouseY < height / 3.5 + height
-      ) {
-        gameMode += 2;
+      } else {
+      if (mouseX > width / 2 && mouseX < width + width / 2 && mouseY > height / 3.5 && mouseY < height / 3.5 + height) {
+        gameState = "numberIns";
       }
     }
+  } else if (gameState === "timerIns") {
+    gameState = "timer"
   }
-  else if (gameMode === 2) {
-    gameMode += 1;
-    }
 }
 
 // Title Screen Function
@@ -55,14 +50,18 @@ function titleScreen() {
   background(51, 153, 255);
 
   // Title Text
-  textSize(80);
-  fill("white")
+  textSize(width * 0.05);
+  fill("white");
   textFont("Verdana");
-  text("Human Benchmark Ripoff", width / 2 - 500, height / 2 - 200);
+  let titleText = "Human Benchmark Ripoff";
+  let titleX = width/2 - textWidth(titleText) / 2; // Center text horizontally
+  text(titleText, titleX, height/2- height * 0.15); // Center text vertically
 
   // Start Button Text
-  textSize(30);
-  text("Click to Start", width / 2 - 100, height / 2 + height / 5.5);
+  textSize(width * 0.03);
+  let startText = "Click to Start";
+  let startX = width / 2 - textWidth(startText) / 2; 
+  text(startText, startX, height / 2 + height * 0.15); 
 }
 
 // Game Started Function
@@ -70,63 +69,69 @@ function gameStarted() {
   background(51, 153, 255);
 
   // Title Text
-  textSize(60);
+  textSize(width * 0.04);
   fill("white");
-  text("Pick Game!", width / 2 - 150, height / 2 - 280);
+  let pickGameText = "Pick Game!";
+  let pickGameX = width/2 - textWidth(pickGameText) / 2; 
+  text(pickGameText, pickGameX, height / 2 - height * 0.35);
 
   // Game Name Text
-  textSize(30);
-  text("Reaction Time", width / 2 - width /3.25 , height / 2 - 200);
-  text("Number Memory", width / 2 + width /5.7, height / 2 - 200);
+  textSize(width * 0.02);
+  let reactionTimeText = "Reaction Time";
+  let numberMemoryText = "Number Memory";
 
-  // Red Rectangle Timer Game
+  let reactionX = width / 2 - width * 0.32
+  let numberX = width / 2 + width * 0.17
+
+  text(reactionTimeText, reactionX, height/2 - height * 0.25);
+  text(numberMemoryText, numberX, height/2 - height * 0.25);
+
+  // Red Rectangle for Timer Game
   fill("red");
   rect(0, height / 3.5, width / 2, height);
 
-  // Blue Rectangle Number Game
+  // Blue Rectangle for Number Game
   fill("blue");
   rect(width / 2, height / 3.5, width / 2, height);
 }
 
-function timerGame() {
-  // Title Text
+function timerGameInstruction() {
   background(51, 153, 255);
-  textSize(60)
-  fill("white")
-  text("Reaction Time Test", width / 2 - 275, height / 2 - 200); 
-  image(thunderImg, width/2- 100, height/2- 150, thunderImg.width/5, thunderImg.height/5);
+
+  // Title Text
+  textSize(height * 0.08); 
+  fill("white");
+  let timerText = "Reaction Time Test";
+  let timerX = width / 2 - textWidth(timerText) / 2;
+  text(timerText, timerX, height / 2 - height * 0.3); 
+  image(thunderImg, width / 2 - thunderImg.width / 9, height / 2 - thunderImg.height / 7, thunderImg.width / 4, thunderImg.height / 4);
 
   // Instructions Text
-  textSize(30);
-  text("When the red screen turns green, click as quickly as you can can.", width/2 - 480, height / 2 + height / 4);
+  textSize(height * 0.04); // 4% of height
+  let instructionText = "When the red screen turns green, click as quickly as you can.";
+  let instructionX = width / 2 - textWidth(instructionText) / 2; 
+  text(instructionText, instructionX, height / 2 + height * 0.2); 
 
   // Start Button Text
-  textSize(30);
-  text("Click to Start", width / 2 - 85, height / 2 + height / 3);
-  
+  textSize(height * 0.03); // 4% of height
+  let startButtonText = "Click to Start";
+  let startButtonX = width / 2 - textWidth(startButtonText) / 2; 
+  text(startButtonText, startButtonX, height / 2 + height * 0.3);
 }
 
-function numberGame() {
+function numberGameIns() {
   background("blue");
 }
 
+// Draw function to manage game screens
 function draw() {
-  text(gameMode, 0, 300);
-  titleScreen();
-
-  // Call for gameStarted Function
-  if (gameMode === 1) {
+  if (gameState === "title") {
+    titleScreen();
+  } else if (gameState === "select") {
     gameStarted();
-  }
-
-  // Call for Timer Game
-  if (gameMode === 2) {
-    timerGame();
-  }
-
-  // Call for Number Game
-  if (gameMode === 3) {
-    numberGame();
-  }
-
+  } else if (gameState === "timerIns") {
+    timerGameInstruction();
+  }else if (gameState === "numberIns") {
+    numberGameIns();
+  }  
 }
