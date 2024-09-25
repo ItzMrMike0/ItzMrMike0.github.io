a4451a13c9b48d51b0bf5dd575683c11e3ccc2b3
Yang2602390 <2602390@spsd.sk.ca>
2024-09-25 13:37:31 -0600

    Updated traffic lights with constants

--- traffic-light/sketch.js
+++ traffic-light/sketch.js
@@ -6,9 +6,9 @@
 // changing according to time. You may want to investigate the millis()
 // function at https://p5js.org/reference/#/p5/millis
 
-let state = "red";
-let waitTime = 3000;
+let state = "green";
 let lastSwitchedTime = 0;
+const LIGHT_DURATION = 3000;
 
 function setup() {
   createCanvas(600, 600);
@@ -17,6 +17,8 @@ function setup() {
 function draw() {
   background(255);
   drawOutlineOfLights();
+  // Cycle colors
+  changeLight();
 }
 
 function drawOutlineOfLights() {
@@ -24,33 +26,31 @@ function drawOutlineOfLights() {
   rectMode(CENTER);
   fill(0);
   rect(width / 2, height / 2, 75, 200, 10);
-  
-  // Cycle colors
-  changeLight();
 }
 
 function changeLight() {
   if (state === "red") {
     fill("red");
     ellipse(width / 2, height / 2 - 65, 50, 50); // Top
-    if (millis() >= lastSwitchedTime + waitTime) {
+    if (millis() >= lastSwitchedTime + LIGHT_DURATION * 1.5 ) {
       lastSwitchedTime = millis();
       state = "green";
     }
-  } else if (state === "green") {
+  } 
+  else if (state === "green") {
     fill("green");
     ellipse(width / 2, height / 2 + 65, 50, 50); // Bottom
-    if (millis() >= lastSwitchedTime + waitTime) {
+    if (millis() >= lastSwitchedTime + LIGHT_DURATION) {
       lastSwitchedTime = millis();
       state = "yellow";
     }
-  } else if (state === "yellow") {
+  } 
+  else if (state === "yellow") {
     fill("yellow");
     ellipse(width / 2, height / 2, 50, 50); // Middle
-    if (millis() >= lastSwitchedTime + waitTime / 1.5) {
+    if (millis() >= lastSwitchedTime + LIGHT_DURATION / 1.5) {
       lastSwitchedTime = millis();
       state = "red";
     }
   }
-
-  } 
+} 
